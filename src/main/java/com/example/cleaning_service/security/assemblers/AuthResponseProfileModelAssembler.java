f72167cc5a26272197c99cba80921c8eff3d0108
phunpt <phu.npt01@gmail.com>
2025-03-09 22:20:33 +0700

    update Auth and remove redundant classes

--- src/main/java/com/example/cleaning_service/security/assemblers/AuthResponseProfileModelAssembler.java
+++ src/main/java/com/example/cleaning_service/security/assemblers/AuthResponseProfileModelAssembler.java
@@ -1,9 +1,10 @@
 package com.example.cleaning_service.security.assemblers;
 
 import com.example.cleaning_service.security.controllers.AuthController;
-import com.example.cleaning_service.security.dtos.auth.AuthResponseProfile;
 import com.example.cleaning_service.security.dtos.auth.AuthResponseProfileModel;
+import com.example.cleaning_service.security.entities.user.User;
 import com.example.cleaning_service.security.mapper.AuthMapper;
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
@@ -11,18 +12,20 @@ import org.springframework.stereotype.Component;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 @Component
-public class AuthResponseProfileModelAssembler extends RepresentationModelAssemblerSupport<AuthResponseProfile, AuthResponseProfileModel> {
+public class AuthResponseProfileModelAssembler extends RepresentationModelAssemblerSupport<User, AuthResponseProfileModel> {
 
     public AuthResponseProfileModelAssembler() {
         super(AuthController.class, AuthResponseProfileModel.class);
     }
 
     @Override
-    public @NonNull AuthResponseProfileModel toModel(@NonNull AuthResponseProfile authResponseProfile) {
-        AuthResponseProfileModel model = AuthMapper.fromAuthResponseProfileToModel(authResponseProfile);
+    public @NonNull AuthResponseProfileModel toModel(@NonNull User user) {
+        AuthResponseProfileModel model = AuthMapper.fromUserToAuthResponseProfileModel(user);
 
         // Add HATEOAS self-link
-        model.add(linkTo(AuthController.class).slash("me").withSelfRel());
+        Link selfLink =  linkTo(AuthController.class).slash("me").withSelfRel();
+        Link logoutLink = linkTo(AuthController.class).slash("logout").withRel("logout");
+        model.add(selfLink, logoutLink);
 
         return model;
     }
--- src/main/java/com/example/cleaning_service/security/assemblers/AuthResponseRegisterModelAssembler.java
+++ src/main/java/com/example/cleaning_service/security/assemblers/AuthResponseRegisterModelAssembler.java
@@ -1,8 +1,8 @@
 package com.example.cleaning_service.security.assemblers;
 
 import com.example.cleaning_service.security.controllers.AuthController;
-import com.example.cleaning_service.security.dtos.auth.AuthResponseRegister;
 import com.example.cleaning_service.security.dtos.auth.AuthResponseRegisterModel;
+import com.example.cleaning_service.security.entities.user.User;
 import com.example.cleaning_service.security.mapper.AuthMapper;
 import org.springframework.hateoas.Link;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
@@ -12,22 +12,26 @@ import org.springframework.stereotype.Component;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 
 @Component
-public class AuthResponseRegisterModelAssembler extends RepresentationModelAssemblerSupport<AuthResponseRegister, AuthResponseRegisterModel> {
+public class AuthResponseRegisterModelAssembler extends RepresentationModelAssemblerSupport<User, AuthResponseRegisterModel> {
 
     public AuthResponseRegisterModelAssembler() {
         super(AuthController.class, AuthResponseRegisterModel.class);
     }
 
     @Override
-    public @NonNull AuthResponseRegisterModel toModel(@NonNull AuthResponseRegister authResponseRegister) {
-        AuthResponseRegisterModel model = AuthMapper.fromAuthResponseRegisterToModel(authResponseRegister);
+    protected @NonNull AuthResponseRegisterModel instantiateModel(@NonNull User user) {
+        return AuthMapper.fromUserToAuthResponseRegisterModel(user);
+    }
+
+    @Override
+    public @NonNull AuthResponseRegisterModel toModel(@NonNull User user) {
+        AuthResponseRegisterModel model = AuthMapper.fromUserToAuthResponseRegisterModel(user);
 
         // Add HATEOAS self-link
-        Link loginLink = linkTo(AuthController.class).slash("register").withSelfRel();
-        Link selfLink = linkTo(AuthController.class).slash("login").withRel("login");
+        Link selfLink =  linkTo(AuthController.class).slash("register").withSelfRel();
+        Link loginLink = linkTo(AuthController.class).slash("login").withRel("login");
 
-        model.add(selfLink);
-        model.add(loginLink);
+        model.add(selfLink, loginLink);
 
         return model;
     }
