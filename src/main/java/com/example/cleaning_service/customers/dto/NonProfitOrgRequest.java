849fdd94956a0747535882bf81c69d41154f6015
phunpt <phu.npt01@gmail.com>
2025-03-11 15:22:56 +0700

    add new requests dto

--- src/main/java/com/example/cleaning_service/customers/dto/NonProfitOrgRequest.java
+++ src/main/java/com/example/cleaning_service/customers/dto/NonProfitOrgRequest.java
@@ -1,4 +1,31 @@
 package com.example.cleaning_service.customers.dto;
 
-public record NonProfitOrgRequest() {
+import com.example.cleaning_service.customers.enums.ECountryType;
+import com.example.cleaning_service.customers.enums.EDay;
+import com.example.cleaning_service.customers.enums.EPaymentType;
+import com.example.cleaning_service.validations.ValidRegistrationNumber;
+import com.example.cleaning_service.validations.ValidTaxId;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.Set;
+
+public record NonProfitOrgRequest(
+        @NotBlank @ValidTaxId String taxId,
+        @NotBlank @ValidRegistrationNumber String registrationNumber,
+
+        String billingAddress,
+        EPaymentType paymentMethod,
+        Set<EDay> preferredDays,
+
+        String organizationName,
+        String address,
+        String phone,
+        String email,
+        String city,
+        String sate,
+        String zip,
+        @NotNull ECountryType country,
+        String notes
+) {
 }
