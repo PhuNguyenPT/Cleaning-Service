c91579b2228ab06208ceb414087a46779f39504f
phunpt <phu.npt01@gmail.com>
2025-03-12 13:53:10 +0700

    fix Repository Long to UUID

--- src/main/java/com/example/cleaning_service/customers/repositories/CompanyRepository.java
+++ src/main/java/com/example/cleaning_service/customers/repositories/CompanyRepository.java
@@ -4,6 +4,8 @@ import com.example.cleaning_service.customers.entities.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface CompanyRepository extends JpaRepository<Company, Long> {
+public interface CompanyRepository extends JpaRepository<Company, UUID> {
 }
--- src/main/java/com/example/cleaning_service/customers/repositories/GovernmentRepository.java
+++ src/main/java/com/example/cleaning_service/customers/repositories/GovernmentRepository.java
@@ -4,6 +4,8 @@ import com.example.cleaning_service.customers.entities.Government;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface GovernmentRepository extends JpaRepository<Government, Long> {
+public interface GovernmentRepository extends JpaRepository<Government, UUID> {
 }
