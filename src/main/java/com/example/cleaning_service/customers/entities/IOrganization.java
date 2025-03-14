872c5ef484d71707ca0c678e6792f8a4c90c1534
phunpt <phu.npt01@gmail.com>
2025-03-14 13:01:36 +0700

    add constraints

--- src/main/java/com/example/cleaning_service/customers/entities/IOrganization.java
+++ src/main/java/com/example/cleaning_service/customers/entities/IOrganization.java
@@ -1,12 +1,14 @@
 package com.example.cleaning_service.customers.entities;
 
 import com.example.cleaning_service.customers.enums.EOrganizationType;
+import com.example.cleaning_service.validations.ValidRegistrationNumber;
+import com.example.cleaning_service.validations.ValidTaxId;
 
 public sealed interface IOrganization permits Company, Government, IndividualCustomer, NonProfitOrg {
     String getTaxId();
-    void setTaxId(String taxId);
+    void setTaxId(@ValidTaxId String taxId);
     String getRegistrationNumber();
-    void setRegistrationNumber(String registrationNumber);
+    void setRegistrationNumber(@ValidRegistrationNumber String registrationNumber);
     EOrganizationType getOrganizationType();
 }
 
