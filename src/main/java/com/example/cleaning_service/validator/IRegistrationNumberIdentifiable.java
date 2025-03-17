a7ad5705fc81a4284bfad9bb7a05c638317dea8f
phunpt <phu.npt01@gmail.com>
2025-03-17 13:15:14 +0700

    rename

--- src/main/java/com/example/cleaning_service/validator/IRegistrationNumberIdentifiable.java
+++ src/main/java/com/example/cleaning_service/validator/IRegistrationNumberIdentifiable.java
@@ -2,7 +2,7 @@ package com.example.cleaning_service.validator;
 
 import com.example.cleaning_service.customers.enums.ECountryType;
 
-public interface RegistrationNumberIdentifiable {
+public interface IRegistrationNumberIdentifiable {
     String getRegistrationNumber();
     ECountryType getCountry();
 }
--- src/main/java/com/example/cleaning_service/validator/ITaxIdentifiable.java
+++ src/main/java/com/example/cleaning_service/validator/ITaxIdentifiable.java
@@ -2,7 +2,7 @@ package com.example.cleaning_service.validator;
 
 import com.example.cleaning_service.customers.enums.ECountryType;
 
-public interface TaxIdentifiable {
+public interface ITaxIdentifiable {
     String getTaxId();
     ECountryType getCountry();
 }
