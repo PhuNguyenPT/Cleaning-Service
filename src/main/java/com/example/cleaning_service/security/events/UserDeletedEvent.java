9a82507a61ca7bd626c35c7ff7d50aea03bf4e34
phunpt <phu.npt01@gmail.com>
2025-03-17 17:42:37 +0700

    remove unused import

--- src/main/java/com/example/cleaning_service/security/events/UserDeletedEvent.java
+++ src/main/java/com/example/cleaning_service/security/events/UserDeletedEvent.java
@@ -1,7 +1,6 @@
 package com.example.cleaning_service.security.events;
 
 import com.example.cleaning_service.security.entities.user.User;
-import org.springframework.context.ApplicationEvent;
 
 public record UserDeletedEvent(User user) {
 }
