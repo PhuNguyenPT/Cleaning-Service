44f2679232cdc0f927fc009f129bb57b63541f21
phunpt <phu.npt01@gmail.com>
2025-03-18 12:21:03 +0700

    remove unused method

--- src/main/java/com/example/cleaning_service/customers/repositories/CompanyRepository.java
+++ src/main/java/com/example/cleaning_service/customers/repositories/CompanyRepository.java
@@ -10,16 +10,6 @@ import java.util.UUID;
 
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, UUID> {
-    boolean existsByTaxIdOrRegistrationNumberOrEmail(@NotBlank String taxId, @NotBlank String registrationNumber, @Email(
-            regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$",
-            message = "Invalid email format. " +
-                    "Expected format: local-part@domain.extension. " +
-                    "- Local part: letters, numbers, dots (.), underscores (_), percent (%), plus (+), or hyphens (-). " +
-                    "- Domain: letters, numbers, dots (.) and hyphens (-). " +
-                    "- Extension: 2 to 6 letters (e.g., .com, .net, .org). " +
-                    "Examples: user@example.com, john.doe@company.org."
-    ) String email);
-
     boolean existsByTaxId(@NotBlank String taxId);
 
     boolean existsByRegistrationNumber(@NotBlank String s);
