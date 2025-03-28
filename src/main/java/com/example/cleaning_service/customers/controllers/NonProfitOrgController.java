07a8ab247394d0b1373480b5df419433cc5b32e3
phunpt <phu.npt01@gmail.com>
2025-03-28 12:31:51 +0700

    fix request mapping

--- src/main/java/com/example/cleaning_service/customers/controllers/NonProfitOrgController.java
+++ src/main/java/com/example/cleaning_service/customers/controllers/NonProfitOrgController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/non-profit-orgs")
+@RequestMapping("/non-profit-organizations")
 public class NonProfitOrgController {
     private final NonProfitOrgService nonProfitOrgService;
 
