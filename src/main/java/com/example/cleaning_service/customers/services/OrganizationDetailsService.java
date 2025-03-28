362f1268a03a26f7c828c6de4efdee6c8f9607d0
phunpt <phu.npt01@gmail.com>
2025-03-28 12:30:53 +0700

    fix null pointer

--- src/main/java/com/example/cleaning_service/customers/services/OrganizationDetailsService.java
+++ src/main/java/com/example/cleaning_service/customers/services/OrganizationDetailsService.java
@@ -4,6 +4,7 @@ import com.example.cleaning_service.customers.controllers.*;
 import com.example.cleaning_service.customers.dto.OrganizationDetailsRequest;
 import com.example.cleaning_service.customers.entities.*;
 import com.example.cleaning_service.customers.enums.EAssociationType;
+import com.example.cleaning_service.security.entities.user.User;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
@@ -50,10 +51,10 @@ public class OrganizationDetailsService {
     Link getLinkByIOrganization(@NotNull IOrganization organization) {
         log.info("Attempting to retrieve link for {}", organization);
         return switch (organization) {
-            case Company company -> linkTo(methodOn(CompanyController.class).getCompanyById(company.getId(), null)).withRel("customer");
-            case IndividualCustomer customer -> linkTo(methodOn(IndividualCustomerController.class).getIndividualCustomerById(customer.getId(), null)).withRel("customer");
-            case Government government -> linkTo(methodOn(GovernmentController.class).getGovernmentById(government.getId(), null)).withRel("customer");
-            case NonProfitOrg nonProfitOrg -> linkTo(methodOn(NonProfitOrgController.class).getNonProfitOrgById(nonProfitOrg.getId(), null)).withRel("customer");
+            case Company company -> linkTo(methodOn(CompanyController.class).getCompanyById(company.getId(), new User())).withRel("customer");
+            case IndividualCustomer customer -> linkTo(methodOn(IndividualCustomerController.class).getIndividualCustomerById(customer.getId(), new User())).withRel("customer");
+            case Government government -> linkTo(methodOn(GovernmentController.class).getGovernmentById(government.getId(), new User())).withRel("customer");
+            case NonProfitOrg nonProfitOrg -> linkTo(methodOn(NonProfitOrgController.class).getNonProfitOrgById(nonProfitOrg.getId(), new User())).withRel("customer");
         };
     }
 }
