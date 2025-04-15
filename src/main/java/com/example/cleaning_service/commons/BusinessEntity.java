b9cfef1d6816198bc801705f72e67b36fcad57e2
phunpt <phu.npt01@gmail.com>
2025-04-15 15:43:12 +0700

    add lombok annotations

--- src/main/java/com/example/cleaning_service/commons/BusinessEntity.java
+++ src/main/java/com/example/cleaning_service/commons/BusinessEntity.java
@@ -1,54 +1,55 @@
 package com.example.cleaning_service.commons;
 
-import com.example.cleaning_service.audit.Auditable;
 import com.example.cleaning_service.customers.enums.ECountryType;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 import java.util.UUID;
 
 @MappedSuperclass
-public class BusinessEntity extends Auditable {
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class BusinessEntity {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id")
     protected UUID id;
 
+    @Column(name = "name", nullable = false)
     protected String name;
+
+    @Column(name = "address")
     protected String address;
 
-    @Pattern(
-            regexp = "^\\+?[1-9]\\d{1,14}$",
-            message = "Invalid international phone number format. " +
-                    "Expected format: optional '+' followed by 2 to 15 digits. " +
-                    "Examples: +1234567890, 1234567890."
-    )
+    @Column(name = "phone")
     protected String phone;
 
-    @Email(
-            regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$",
-            message = "Invalid email format. " +
-                    "Expected format: local-part@domain.extension. " +
-                    "- Local part: letters, numbers, dots (.), underscores (_), percent (%), plus (+), or hyphens (-). " +
-                    "- Domain: letters, numbers, dots (.) and hyphens (-). " +
-                    "- Extension: 2 to 6 letters (e.g., .com, .net, .org). " +
-                    "Examples: user@example.com, john.doe@company.org."
-    )
+    @Column(name = "email")
     protected String email;
 
+    @Column(name = "city")
     protected String city;
+
+    @Column(name = "state")
     protected String state;
+
+    @Column(name = "zip")
     protected String zip;
 
     @Enumerated(EnumType.STRING)
+    @Column(name = "country")
     protected ECountryType country;
 
+    @Column(name = "notes")
     protected String notes;
 
-    public BusinessEntity() {
-    }
-
-    public BusinessEntity(String name, String address, String phone, String email, String city, String state, String zip, ECountryType country, String notes) {
+    public BusinessEntity(String name, String address, String phone, String email, String city, String state,
+                          String zip, ECountryType country, String notes) {
         this.name = name;
         this.address = address;
         this.phone = phone;
@@ -58,98 +59,5 @@ public class BusinessEntity extends Auditable {
         this.zip = zip;
         this.country = country;
         this.notes = notes;
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getZip() {
-        return zip;
-    }
-
-    public void setZip(String zip) {
-        this.zip = zip;
-    }
-
-
-    public String getNotes() {
-        return notes;
-    }
-
-    public void setNotes(String notes) {
-        this.notes = notes;
-    }
-
-    public ECountryType getCountry() {
-        return country;
-    }
-
-    public void setCountry(ECountryType country) {
-        this.country = country;
-    }
-
-    @Override
-    public String toString() {
-        return "BusinessEntity{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", address='" + address + '\'' +
-                ", phone='" + phone + '\'' +
-                ", email='" + email + '\'' +
-                ", city='" + city + '\'' +
-                ", state='" + state + '\'' +
-                ", zip='" + zip + '\'' +
-                ", country=" + country +
-                ", notes='" + notes + '\'' +
-                '}';
     }
 }
