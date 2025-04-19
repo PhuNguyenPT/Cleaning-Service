8f461a24fd9b3c5a3ea60c5cd8fdde01149926c4
phunpt <phu.npt01@gmail.com>
2025-04-19 17:57:12 +0700

    fix scripts to have same as docker hub

--- scripts/build_app_image.sh
+++ scripts/build_app_image.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo "Build app image for different dockerfiles..."
-docker build -t app-image:ubuntu -f Dockerfile-ubuntu-jammy-eclipse-temurin-jammy .
-docker build -t app-image:alpine -f Dockerfile-alpine-eclipse-temurin-alpine .
-docker build -t app-image:bookworm-slim-corretto -f Dockerfile-bookworm-slim-amazon-corretto .
-docker build -t app-image:bookworm-slim-eclipse-temurin -f Dockerfile-bookworm-slim-eclipse-temurin .
+docker build -t phunpt01/app-image:jammy -f Dockerfile-ubuntu-jammy-eclipse-temurin-jammy .
+docker build -t phunpt01/app-image:alpine -f Dockerfile-alpine-eclipse-temurin-alpine .
+docker build -t phunpt01/app-image:corretto-bookworm-slim -f Dockerfile-bookworm-slim-amazon-corretto .
+docker build -t phunpt01/app-image:eclipse-temurin-bookworm-slim -f Dockerfile-bookworm-slim-eclipse-temurin .
--- scripts/docker_push_image.sh
+++ scripts/docker_push_image.sh
@@ -1,14 +1,9 @@
 #!/bin/sh
 
 echo "Push all images to docker hub..."
-# Tag
-docker tag app-image:ubuntu phunpt01/app-image:ubuntu
-docker tag app-image:alpine phunpt01/app-image:alpine
-docker tag app-image:bookworm-slim-corretto phunpt01/app-image:bookworm-slim-corretto
-docker tag app-image:bookworm-slim-eclipse-temurin phunpt01/app-image:bookworm-slim-eclipse-temurin
 
 # Push
-docker push phunpt01/app-image:ubuntu
+docker push phunpt01/app-image:jammy
 docker push phunpt01/app-image:alpine
-docker push phunpt01/app-image:bookworm-slim-corretto
-docker push phunpt01/app-image:bookworm-slim-eclipse-temurin
+docker push phunpt01/app-image:corretto-bookworm-slim
+docker push phunpt01/app-image:eclipse-temurin-bookworm-slim
