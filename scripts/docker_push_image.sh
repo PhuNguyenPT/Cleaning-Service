029ec0b272b42a98c42dc42f914d882eb1e6d450
phunpt <phu.npt01@gmail.com>
2025-04-19 18:25:57 +0700

    update scripts for jib

--- scripts/docker_push_image.sh
+++ scripts/docker_push_image.sh
@@ -7,3 +7,7 @@ docker push phunpt01/app-image:jammy
 docker push phunpt01/app-image:alpine
 docker push phunpt01/app-image:corretto-bookworm-slim
 docker push phunpt01/app-image:eclipse-temurin-bookworm-slim
+docker push phunpt01/app-image:jib-eclipse-temurin-21-jre
+docker push phunpt01/app-image:jib-eclipse-temurin-21-jre-jammy
+docker push phunpt01/app-image:jib-amazoncorretto-21.0.7
+docker push phunpt01/app-image:jib-amazoncorretto-21
