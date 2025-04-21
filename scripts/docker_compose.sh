73e4d46c2f2c49803384063c2f62378f9f42b058
phunpt <phu.npt01@gmail.com>
2025-04-21 15:33:30 +0700

    fix app to run in docker, add scripts

--- scripts/docker_compose.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-chmod +x scripts/update_run.sh
-# Start all services
-echo "Starting all services..."
-docker compose -f docker-compose.yaml up -d
--- scripts/docker_rebuild_app_image.sh
+++ scripts/docker_rebuild_app_image.sh
@@ -4,6 +4,12 @@ chmod +x scripts/build_app_image.sh
 ./scripts/build_app_image.sh
 
 echo "Remove old service and compose"
-docker compose -f docker-compose.yaml stop app
-docker compose -f docker-compose.yaml rm -f app
-docker compose -f docker-compose.yaml up -d app
+# docker file image
+docker compose --env-file docker-compose.env -p cleaning-service-dockerfile -f docker-compose.yaml stop app
+docker compose --env-file docker-compose.env -p cleaning-service-dockerfile -f docker-compose.yaml  rm -f app
+docker compose --env-file docker-compose.env -p cleaning-service-dockerfile -f docker-compose.yaml up -d app
+
+# jib image
+#docker compose --env-file docker-compose-jib.env -p cleaning-service-jib -f docker-compose-jib.yaml stop app
+#docker compose --env-file docker-compose-jib.env -p cleaning-service-jib -f docker-compose-jib.yaml  rm -f app
+#docker compose --env-file docker-compose-jib.env -p cleaning-service-jib -f docker-compose-jib.yaml up -d app
