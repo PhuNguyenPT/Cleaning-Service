9b1e5f326990707d3c3071b009feefd0947b3f58
phunpt <phu.npt01@gmail.com>
2025-04-24 18:10:46 +0700

    remove sha

--- scripts/build_app_image.sh
+++ scripts/build_app_image.sh
@@ -7,19 +7,19 @@ docker build -t phunpt01/app-image:corretto-bookworm-slim -f Dockerfile-bookworm
 docker build -t phunpt01/app-image:eclipse-temurin-bookworm-slim -f Dockerfile-bookworm-slim-eclipse-temurin .
 
 ./mvnw compile jib:dockerBuild \
-  -Djib.from.image=eclipse-temurin:21-jre@sha256:9fe87739492c449b9d4e036ac0a79a7e2c75ce36c99d4ba4a0666a5d4e409e73 \
+  -Djib.from.image=eclipse-temurin:21-jre\
   -Djib.to.image=docker.io/phunpt01/app-image:jib-eclipse-temurin-21-jre \
   -Djib.container.jvmFlags="-XX:+UseParallelGC,-XX:MaxRAMPercentage=75" \
   -Djib.from.platforms=linux/amd64
 
 ./mvnw compile jib:dockerBuild \
-  -Djib.from.image=eclipse-temurin:21-jre-jammy@sha256:903dcea12637919198041da42cd270d549656c06bfda9126be36f182bb0de72d \
+  -Djib.from.image=eclipse-temurin:21-jre-jammy \
   -Djib.to.image=docker.io/phunpt01/app-image:jib-eclipse-temurin-21-jre-jammy \
   -Djib.container.jvmFlags="-XX:+UseParallelGC,-XX:MaxRAMPercentage=75" \
   -Djib.from.platforms=linux/amd64
 
 ./mvnw compile jib:dockerBuild \
-  -Djib.from.image=amazoncorretto:21@sha256:674d24b818b8e889fb530a98c250f8638f14116f34eddbe0191f74e4dbc12aa0 \
+  -Djib.from.image=amazoncorretto:21 \
   -Djib.to.image=docker.io/phunpt01/app-image:jib-amazoncorretto-21 \
   -Djib.container.jvmFlags="-XX:+UseParallelGC,-XX:MaxRAMPercentage=75" \
   -Djib.from.platforms=linux/amd64
