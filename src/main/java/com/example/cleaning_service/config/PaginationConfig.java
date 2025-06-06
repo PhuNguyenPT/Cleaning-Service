73ed5fc3f2d12c90d2bf32f618f65b6ebe30dcac
phunpt <phu.npt01@gmail.com>
2025-06-06 13:06:22 +0700

    remove unused

--- src/main/java/com/example/cleaning_service/config/PaginationConfig.java
+++ src/main/java/com/example/cleaning_service/config/PaginationConfig.java
@@ -1,10 +1,7 @@
 package com.example.cleaning_service.config;
 
-import com.example.cleaning_service.customers.entities.Account;
-import com.example.cleaning_service.security.entities.user.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.data.web.config.PageableHandlerMethodArgumentResolverCustomizer;
 
 @Configuration
